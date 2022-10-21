c840bb55788747fb2760d5bdb67dc96b391e5a55
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2022-10-21 17:52:21 -0300

    removido variaveis desnecessarias

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,29 +1,26 @@
 const {Psicologos, Atendimento, Pacientes} = require('../models/index');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const secret = require('../middlewares/secret');
 
 const dashboardController = {
     async numeroPacientes (req, res){
-        const {count, rows} = await Pacientes.findAndCountAll();
+        const {count } = await Pacientes.findAndCountAll();
 
         return res.status(201).json(count);
     },
 
     async numeroAtendimentos (req, res){
-        const {count, rows} = await Atendimento.findAndCountAll();
+        const {count } = await Atendimento.findAndCountAll();
 
         return res.status(201).json(count);
     },
     async numeroPsicologos (req, res){
-        const {count, rows} = await Psicologos.findAndCountAll();
+        const {count } = await Psicologos.findAndCountAll();
 
         return res.status(201).json(count);
     },
     async mediaPorPsicologo (req, res){
-        const {count: quantAtendimentos, rows: atendimentos} = await Atendimento.findAndCountAll();
+        const {count: quantAtendimentos} = await Atendimento.findAndCountAll();
 
-        const {count: quantPsicologos, rows: psicologos} = await Psicologos.findAndCountAll();
+        const {count: quantPsicologos} = await Psicologos.findAndCountAll();
 
         res.status(201).json(quantAtendimentos/quantPsicologos);
     },
