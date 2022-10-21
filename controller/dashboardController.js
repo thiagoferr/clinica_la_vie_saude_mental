762d096a1881a4619c0e1b62d7ff9ca7d79fba8d
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2022-10-21 17:54:03 -0300

    corrigido send

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -4,25 +4,25 @@ const dashboardController = {
     async numeroPacientes (req, res){
         const {count } = await Pacientes.findAndCountAll();
 
-        return res.status(201).json(count);
+        return res.status(201).send(count);
     },
 
     async numeroAtendimentos (req, res){
         const {count } = await Atendimento.findAndCountAll();
 
-        return res.status(201).json(count);
+        return res.status(201).send(count);
     },
     async numeroPsicologos (req, res){
         const {count } = await Psicologos.findAndCountAll();
 
-        return res.status(201).json(count);
+        return res.status(201).send(count);
     },
     async mediaPorPsicologo (req, res){
         const {count: quantAtendimentos} = await Atendimento.findAndCountAll();
 
         const {count: quantPsicologos} = await Psicologos.findAndCountAll();
 
-        res.status(201).json(quantAtendimentos/quantPsicologos);
+        res.status(201).send(quantAtendimentos/quantPsicologos);
     },
 }
 
